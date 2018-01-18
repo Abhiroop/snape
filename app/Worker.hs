d52ab522ceb731dc4e62a70c38e155f7729c5067
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-01-18 20:38:07 +0000

    State reporting code among the workers

--- app/Worker.hs
+++ app/Worker.hs
@@ -12,40 +12,49 @@ import Data.Binary (Binary)
 import GHC.Generics (Generic)
 import Data.Typeable (Typeable)
 
-type QueueLength = Int
-
 data QueueState = Empty | Processing
   deriving (Show, Generic, Typeable)
 
-data Message = Message { senderOf    :: ProcessId
-                       , recipientOf :: ProcessId
-                       , msg         :: QueueState
-                       } deriving (Show, Generic, Typeable)
+data MessageTypes = MyCapacity { senderOf    :: ProcessId
+                               , recipientOf :: ProcessId
+                               , msg         :: QueueState
+                               , workLoad    :: Int
+                               } deriving (Show, Generic, Typeable)
+-- add more messages in futures
+-- 1. work stealing from worket to peers
 
 data WorkerConfig = WorkerConfig { master      :: ProcessId
                                  , myId        :: ProcessId
                                  , peers       :: [ProcessId] -- this will be useful for work stealing later
-                                 , queueLength :: Integer
                                  }
 
-data WorkerState = forall a . WorkerState { taskQueue :: IO (InChan a, OutChan a)
+data WorkerState = forall a . WorkerState { taskQueue   :: IO (InChan a, OutChan a)
+                                          , queueLength :: Int
                                           }
 
-newtype WorkerAction a = WorkerAction {runAction :: RWS WorkerConfig [Message] WorkerState a
+newtype WorkerAction a = WorkerAction {runAction :: RWS WorkerConfig [MessageTypes] WorkerState a
                                       } deriving (Functor,
                                                   Applicative,
                                                   Monad,
                                                   MonadState WorkerState,
-                                                  MonadWriter [Message],
+                                                  MonadWriter [MessageTypes],
                                                   MonadReader WorkerConfig)
 
-initWorker :: QueueLength -> IO (InChan a, OutChan a)
-initWorker = newChan
+initWorker :: WorkerState -> IO (InChan a, OutChan a)
+initWorker (WorkerState _ l) = newChan l
 
 submitWork :: a -> IO Bool
 submitWork = undefined
 
---askForWork :: 
+reportState :: WorkerAction ()
+reportState = do
+  WorkerConfig m me _ <- ask
+  WorkerState _ l    <- get
+  if (l == 0)
+    then tell [MyCapacity me m Empty 0]
+    else tell [MyCapacity me m Processing l]
+
+
 test :: IO ()
 test = do
   (inC, outC) <- newChan 5 
