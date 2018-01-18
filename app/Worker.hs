37d2c1982cec5c6c45a9afa5a3037ebb8b8fe79b
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-01-18 16:17:24 +0000

    Add mtl

--- app/Worker.hs
+++ app/Worker.hs
@@ -4,5 +4,24 @@ import Control.Concurrent.Chan.Unagi.Bounded
 
 type QueueLength = Int
 
-initWorker :: QueueLength -> IO ()
-initWorker n = undefined
+data WorkerState a = WorkerState { taskQueue :: (InChan a, OutChan a)
+                                 }
+
+initWorker :: QueueLength -> IO (InChan a, OutChan a)
+initWorker = newChan
+
+submitWork :: a -> IO Bool
+submitWork = undefined
+
+test :: IO ()
+test = do
+  (inC, outC) <- newChan 5 -- :: IO (InChan a, OutChan a)
+  writeChan inC 6
+  writeChan inC 7
+  writeChan inC 8
+  x <- readChan outC
+  print x
+  n <- estimatedLength inC
+  print n
+  return ()
+
