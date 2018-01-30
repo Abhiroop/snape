1390e773390817b2028f8480fe7d4b4fd7e88887
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-01-30 19:20:24 +0000

    Add the static pointer to capture the functions

--- app/Worker.hs
+++ app/Worker.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE StaticPointers #-}
 module Worker where
 
 import Control.Distributed.Process (ProcessId, Process)
@@ -9,18 +10,19 @@ import Control.Concurrent.Chan.Unagi.Bounded
 import Control.Monad.RWS.Strict
 
 import Data.Binary (Binary)
-import GHC.Generics (Generic)
 import Data.Typeable (Typeable)
+import GHC.Generics (Generic)
+import GHC.StaticPtr
 
 import Queue
 -- Limiting the kind of tasks possible right now but will extend this in future to include all kinds of Haskell tasks
 -- TODO: Look at LINQ operators for this function
 -- a is the serialized function to be applied here Eg: (+ 1)
-data Task a = Map a
-            | Filter a
-            | Reduce a
-            | GroupBy a
-            deriving (Show, Generic, Typeable)
+data Task a b = Map    (StaticPtr (a -> b))
+              | Filter (StaticPtr (a -> Bool))
+              | Reduce (StaticPtr (a -> b -> b) -> b)
+              | GroupBy a
+              deriving (Generic, Typeable)
 
 data QueueState = Empty
                 | Processing
@@ -28,7 +30,7 @@ data QueueState = Empty
 
 -- TODO: This should be moved to a separate file Messages.hs
 -- a is the serialized function to be applied
-data Messages a = WorkerCapacity { senderOf    :: ProcessId
+data Messages a b = WorkerCapacity { senderOf    :: ProcessId
                                  , recipientOf :: ProcessId
                                  , msg         :: QueueState
                                  , workLoad    :: Int
@@ -36,9 +38,9 @@ data Messages a = WorkerCapacity { senderOf    :: ProcessId
                 -- the worker pushes this every n secs to the scheduler.
                 | WorkerTask { senderOf    :: ProcessId
                              , recipientOf :: ProcessId
-                             , work        :: Task a}
+                             , work        :: Task a b}
                 -- the master sends this to the worker every time a worker returns that it is empty
-                deriving (Show, Generic, Typeable)
+                deriving (Generic, Typeable)
 -- add more messages in futures
 -- 1. work stealing from worker to peers
 
@@ -47,33 +49,33 @@ data WorkerConfig = WorkerConfig { master      :: ProcessId
                                  , peers       :: [ProcessId] -- this will be useful for work stealing later
                                  }
 
-data WorkerState a = WorkerState { taskQueue   :: Queue (Task a)
+data WorkerState a b = WorkerState { taskQueue   :: Queue (Task a b)
                                  , queueLength :: Int
                                  }
 
 
 
-newtype WorkerAction m a = WorkerAction {runAction :: RWS WorkerConfig [Messages m] (WorkerState m) a
+newtype WorkerAction t m a = WorkerAction {runAction :: RWS WorkerConfig [Messages t m] (WorkerState t m) a
                                         } deriving (Functor,
                                                     Applicative,
                                                     Monad,
-                                                    MonadState (WorkerState m),
-                                                    MonadWriter [Messages m],
+                                                    MonadState (WorkerState t m),
+                                                    MonadWriter [Messages t m],
                                                     MonadReader WorkerConfig)
 
 
-initWorker :: WorkerState a -> IO (InChan a, OutChan a)
+initWorker :: WorkerState a b -> IO (InChan a, OutChan a)
 initWorker (WorkerState _ l) = newChan l
 
 submitWork :: a -> IO Bool
 submitWork = undefined
 
-taskSubmissionHandler :: Messages m -> WorkerAction m ()
+taskSubmissionHandler :: Messages t m -> WorkerAction t m ()
 taskSubmissionHandler (WorkerTask _ _ t ) = do
   WorkerState q l  <- get
   put $ WorkerState (t >>| q) (l + 1)
 
-reportState :: WorkerAction m ()
+reportState :: WorkerAction t m ()
 reportState = do
   WorkerConfig m me _ <- ask
   WorkerState _ l    <- get
@@ -82,8 +84,12 @@ reportState = do
     else tell [WorkerCapacity me m Processing l]
 
 -- This function actually applies the function to the data
-foo :: Task a -> FilePath -> IO ()
-foo = undefined
+apply :: (Num a) => Task a b -> FilePath -> IO ()
+apply (Map f) filepath = do
+  let func = deRefStaticPtr f
+  let k = func 10
+  return ()
+
 
 test :: IO ()
 test = do
